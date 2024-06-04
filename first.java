484490c6ba431a9686c65518fdc9f1dc07efbba7
Helios-rk <94742478+Helios-rk@users.noreply.github.com>
2024-06-04 17:38:37 +0530

    Update first.java

--- first.java
+++ first.java
@@ -2,6 +2,12 @@ public class first {
 
     public static void main(String[] args) {
 		System.out.println("Hercules");
+	    if(n==2){
+	    System.out.prinln("number is even");
+	    }
+	    else{
+		    System.out.println("number is not even");
+	    }
 
 	}
 }
